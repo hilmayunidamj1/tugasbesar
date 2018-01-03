d4582c17c42a5caeab18958c025d20e77e3614f3
hilmayunidamj1 <32450192+hilmayunidamj1@users.noreply.github.com>
2018-01-03 05:14:28 -0800

    bismillah

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -23,6 +23,19 @@ int main(){
 		cout << a3c[kuda3]<<" | ";
 		kuda3++;
 	}while(kuda3<5);cout<<endl;
+	//r
+	double mc1=max(a1c[0],max(a2c[0],a3c[0])),
+	mc2=max(a1c[1],max(a2c[1],a3c[1])),
+	mc3=max(a1c[2],max(a2c[2],a3c[2])),
+	mc4=max(a1c[3],max(a2c[3],a3c[3])),
+	mc5=max(a1c[4],max(a2c[4],a3c[4]));
+	
+	double Ra1c[5]={ (a1c[0]/mc1),(a1c[1]/mc2),(a1c[2]/mc3),(a1c[3]/mc4),(a1c[4]/mc5) };
+	double Ra2c[5]={ (a2c[0]/mc1),(a2c[1]/mc2),(a2c[2]/mc3),(a2c[3]/mc4),(a2c[4]/mc5) };
+	double Ra3c[5]={ (a3c[0]/mc1),(a3c[1]/mc2),(a3c[2]/mc3),(a3c[3]/mc4),(a3c[4]/mc5) };
+	//r
+		cout << endl<<endl;
+		cout << "MATRIX R :"<<endl;
 	
 	
 	
---
+++ Untitled2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	//x
+	double a1c[5]={4,4,5,3,3}, a2c[5]={3,3,4,2,3}, a3c[5]={5,4,2,2,2};
+	//x
+	cout << "MATRIX X -:"<<endl;
