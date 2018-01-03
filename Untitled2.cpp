1efbaa3c505b163e0e5859f8fde10f1c3ebecd7f
hilmayunidamj1 <32450192+hilmayunidamj1@users.noreply.github.com>
2018-01-03 05:29:07 -0800

    alhamdulillahakhirnya

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -76,4 +76,16 @@ int main(){
 		cout << RWa3c[domba3]<<" | ";
 		domba3++;
 	}while(domba3<5);cout<<endl;
+		//hasil
+	double ha1[5]={ RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4] };
+	double ha2[5]={ RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4] };
+	double ha3[5]={ RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4] };	
+	 
+	cout<< endl << "nilai akhir"<< endl;
+	cout << "Nilai A1 : "<<ha1[0]<<endl;
+	cout << "Nilai A2 : "<<ha2[0]<<endl;
+	cout << "Nilai A3 : "<<ha3[0]<<endl;
+	
+	//[] menggunakan array 1 dimensi
+	 
 }
